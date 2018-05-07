5a38dbce3dd7220542c6639ec4cf6f90f79cf7d4
deb <debkbanerji@gmail.com>
2018-05-07 15:16:04 +0530

    Add http error handling

--- static/where-to-eat/src/app/app.component.ts
+++ static/where-to-eat/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'app-root',
@@ -7,12 +8,16 @@ import {Component, OnInit} from '@angular/core';
 })
 export class AppComponent implements OnInit {
     public pageState: string = 'beforeSearch';
+    public httpErrorMessage: string;
 
     public targetLatitude: number;
     public targetLongitude: number;
     public targetLocation: string;
 
     private API_ADDRESS = '/api/nearby-places';
+
+    constructor(private http: HttpClient) {
+    }
 
     ngOnInit(): void {
         this.setCoordinatesToProvided();
@@ -36,6 +41,29 @@ export class AppComponent implements OnInit {
 
     public doSeearch() {
         const component = this;
-        component.pageState = 'loading'
+        component.pageState = 'loading';
+
+        const requestParams = {};
+
+        const requestParamsKeys = Object.keys(requestParams);
+        const requestParamsList = [];
+        for (let i = 0; i < requestParamsKeys.length; i++) {
+            const key = requestParamsKeys[i];
+            requestParamsList.push(key + '=' + requestParams[key]);
+        }
+
+        const targetAddress = this.API_ADDRESS + '?' + requestParamsList.join('&');
+
+        console.log('Making request', targetAddress);
+
+        this.http.get(targetAddress).subscribe(response => {
+            console.log('response', response);
+            component.httpErrorMessage = null;
+            this.pageState = 'afterSearch';
+        }, (error => {
+            console.log('Error connecting to API', error);
+            component.httpErrorMessage = error.message;
+            this.pageState = 'beforeSearch';
+        }));
     }
 }
--- static/where-to-eat/src/app/app.module.ts
+++ static/where-to-eat/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {AppComponent} from './app.component';
 import {MatButtonModule, MatCardModule, MatInputModule, MatProgressSpinnerModule} from "@angular/material";
 import {FormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        HttpClientModule,
         FormsModule,
         MatCardModule,
         MatInputModule,
