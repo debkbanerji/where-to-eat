9bf7446c60973050dc293b8f0e859305dc257c94
deb <debkbanerji@gmail.com>
2018-05-07 13:56:30 +0530

    Add basic location form

--- static/where-to-eat/src/app/app.component.ts
+++ static/where-to-eat/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-    title = 'app';
+export class AppComponent implements OnInit {
+    public pageState: string = 'beforeQuery';
+
+    public targetLatitude: number;
+    public targetLongitude: number;
+    public targetLocation: string;
+
+    ngOnInit(): void {
+        this.setCoordinatesToProvided();
+    }
+
+    private setCoordinatesToProvided(): void {
+        const component = this;
+        navigator.geolocation.getCurrentPosition(function (providedLocation) {
+            const providedCoordinates = providedLocation.coords;
+            component.targetLatitude = providedCoordinates.latitude;
+            component.targetLongitude = providedCoordinates.longitude;
+            console.log('User\'s estimated location', providedLocation)
+        }, function () {
+            // Do nothing - user must enter location manually
+        });
+    }
 }
